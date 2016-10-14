3b5998583d31b464a01d33d48e5c30cc9571e369
cmgt <porotikovdo@gmail.com>
2016-10-14 16:36:36 +0300

    ida utils

--- EsIDAUtils/EsIDAUtils/ida_helper.py
+++ EsIDAUtils/EsIDAUtils/ida_helper.py
@@ -9,9 +9,9 @@ from PySide import QtGui
 def vector_editor_show():
     print("Hotkey activated!")
     global vector;
-    vector_descr = vector_descr(u"Test", axis("rpm", 0x800000), 0x800000, 10, category="es_fuel_supply", comment=u"Test\ncomment")
+    vdescr = vector_descr(u"Test", axis("rpm", 0x800000), 0x800000, 10, category="es_fuel_supply", comment=u"Test\ncomment")
     vector = vector_editor()
-    vector.show(vector_descr)
+    vector.show(vdescr)
     print(vector.result)
 
 def es_init():
@@ -20,6 +20,8 @@ def es_init():
     idaapi.CompileLine('static vector_editor() { RunPythonStatement("vector_editor_show()"); }')
     # Add the hotkey
     AddHotkey("Ctrl-2", 'vector_editor')
+
+    #ex_addmenu_item_ctx = idaapi.add_menu_item("Edit/", "Show vector editor", "", 0, vector_editor_show, None)
 
 def es_clear():
     DelHotkey('Ctrl-2')
