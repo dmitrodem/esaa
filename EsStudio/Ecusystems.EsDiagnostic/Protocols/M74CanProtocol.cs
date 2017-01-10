eb836652bc0cd61c5e2e0c7b93ba8fdd3966f296
cmgt <porotikovdo@gmail.com>
2017-01-10 15:54:38 +0300

    ecu console

--- EsStudio/Ecusystems.EsDiagnostic/Protocols/M74CanProtocol.cs
+++ EsStudio/Ecusystems.EsDiagnostic/Protocols/M74CanProtocol.cs
@@ -7,11 +7,13 @@ using Ecusystems.Common;
 
 namespace Ecusystems.EsDiagnosticModule.Protocols
 {
-    internal static class M74CanDiagCmd
+    internal static class M74CanProtocol
     {
         /// <summary>
         /// PID - 22. 1, 2, 3, 5, 6, 7, 8, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 9A, A0, A1, A2, A3, E0, E1, 80, A, B
         /// </summary>
-        public static readonly RequestObject GetCommonParameters = new RequestObject(new byte[] { 0x22, 0x00, 0x01, 0x69 }, new byte[] { 0x62, 0x00, 0x01, 0x69 });
+        public static readonly RequestObject GetCommonParameters = new RequestObject(new byte[] { 0x22, 0x00, 0x01}, new byte[] { 0x62, 0x00, 0x01});
+
+        public static readonly RequestObject GetSupportedPids = new RequestObject(new byte[] {0x01, 0x00}, new byte[] {0x61});
     }
 }
